4e0319e4f73a659194521c254b9d75d6e895c1f8
Caroline R. Jones <55961845+Carolista@users.noreply.github.com>
2025-06-08 01:07:06 -0400

    updated links to images

--- script.js
+++ script.js
@@ -23,32 +23,32 @@ window.addEventListener('load', () => {
 		d20: { 
 			value: 20, 
 			label: 'D20', 
-			image: 'https://drive.google.com/uc?export=view&id=1Cdv19qd-HBZSnrfe2hkunU3bsPZ7VPht',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1Cdv19qd-HBZSnrfe2hkunU3bsPZ7VPht',
 		},
 		d12: { 
 			value: 12, 
 			label: 'D12', 
-			image: 'https://drive.google.com/uc?export=view&id=1gRMfOlt98M5dmc-Ex5enZRFy7Bkkr9ta',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1gRMfOlt98M5dmc-Ex5enZRFy7Bkkr9ta',
 		},
 		d10: { 
 			value: 10, 
 			label: 'D10', 
-			image: 'https://drive.google.com/uc?export=view&id=1iwbOiPeNMREcQswWeLZ-dDJXzoaozVum',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1iwbOiPeNMREcQswWeLZ-dDJXzoaozVum',
 		},
 		d8: { 
 			value: 8, 
 			label: 'D8', 
-			image: 'https://drive.google.com/uc?export=view&id=1LrjniIM1RG4u8as6hT5Pa-nS_1z0Bt5z',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1LrjniIM1RG4u8as6hT5Pa-nS_1z0Bt5z',
 		},
 		d6: { 
 			value: 6, 
 			label: 'D6', 
-			image: 'https://drive.google.com/uc?export=view&id=1KABrJFwctJG8JlLhL8ZXYIjj4-a2Gd64',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1KABrJFwctJG8JlLhL8ZXYIjj4-a2Gd64',
 		},
 		d4: { 
 			value: 4, 
 			label: 'D4', 
-			image: 'https://drive.google.com/uc?export=view&id=1TywBsbvDZ8IMiz_7UNQJJS88FTd2kDTN',
+			image: 'https://drive.google.com/thumbnail?size=400&id=1TywBsbvDZ8IMiz_7UNQJJS88FTd2kDTN',
 		},
 	};
 
