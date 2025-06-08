684d145f1be935912f5629d8ee028edbde556ef1
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-08 16:49:07 -0400

    updated image references

--- dice.js
+++ dice.js
@@ -2,32 +2,32 @@ const dice = {
 	d20: { 
 		value: 20, 
 		label: 'D20', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1Cdv19qd-HBZSnrfe2hkunU3bsPZ7VPht',
+		image: './images/d20-white.png',
 	},
 	d12: { 
 		value: 12, 
 		label: 'D12', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1gRMfOlt98M5dmc-Ex5enZRFy7Bkkr9ta',
+		image: './images/d12-white.png',
 	},
 	d10: { 
 		value: 10, 
 		label: 'D10', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1iwbOiPeNMREcQswWeLZ-dDJXzoaozVum',
+		image: './images/d10-white.png',
 	},
 	d8: { 
 		value: 8, 
 		label: 'D8', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1LrjniIM1RG4u8as6hT5Pa-nS_1z0Bt5z',
+		image: './images/d8-white.png',
 	},
 	d6: { 
 		value: 6, 
 		label: 'D6', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1KABrJFwctJG8JlLhL8ZXYIjj4-a2Gd64',
+		image: './images/d6-white.png',
 	},
 	d4: { 
 		value: 4, 
 		label: 'D4', 
-		image: 'https://drive.google.com/thumbnail?size=400&id=1TywBsbvDZ8IMiz_7UNQJJS88FTd2kDTN',
+		image: './images/d4-white.png',
 	},
 };
 
